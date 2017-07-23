3392f1e25a22b6c17f8b6d5cb1fd09d13a8b59ac
sofild <609263898@qq.com>
2017-07-23 13:54:20 +0800

    i try...

---
+++ Itry/cgroup.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"syscall"
+)
+
+const cgroupMemoryHierarchyMount = "/sys/fs/cgroup/memory"
+
+func main() {
+	if os.Args[0] == "/proc/self/exe" {
+
+		fmt.Printf("current pid %d", syscall.Getpid())
+		fmt.Println()
+
+		cmd := exec.Command("sh", "-c", `stress --vm-bytes 200m --vm-keep -m 1`)
+		cmd.SysProcAttr = &syscall.SysProcAttr{}
+		//cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(1), Gid: uint32(1)}
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		if err := cmd.Run(); err != nil {
+			log.Fatal(err)
+			os.Exit(-1)
+		}
+	}
+
+	cmd := exec.Command("/proc/self/exe")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Start(); err != nil {
+		fmt.Println("Error", err)
+		os.Exit(1)
+	} else {
+		fmt.Printf("%v", cmd.Process.Pid)
+		os.Mkdir(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "tasks"), []byte("100m"), 0644)
+	}
+	cmd.Process.Wait()
+}
